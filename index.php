0e573c0b8110fad890714a6e16e1190a4b69f738
Israel Medina <isra2dave@gmail.com>
2017-10-16 02:43:52 -0300

    Stylized new landing page

--- index.php
+++ index.php
@@ -4,9 +4,14 @@
     <meta charset="utf-8">
     <title>Israel Medina</title>
     <link rel="stylesheet" href="/static/css/master.css">
+    <link href="https://fonts.googleapis.com/css?family=Raleway|Roboto" rel="stylesheet">
   </head>
   <body>
-      <h1>Israel Medina</h1>
-      <p>Site under construction</p>
+    <div class="general">
+      <div class="item">
+        <h1>Israel Medina</h1>
+        <p>Site under construction</p>
+      </div>
+    </div>
     </body>
 </html>
