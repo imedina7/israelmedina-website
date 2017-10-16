c3d4b1a8b160f91dbbfa5a186210294fefd342d4
Israel Medina <isra2dave@gmail.com>
2017-10-16 02:14:58 -0300

    removed node app, added php app

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Israel Medina</title>
+    <link rel="stylesheet" href="/static/css/master.css">
+  </head>
+  <body>
+      <h1>Israel Medina</h1>
+      <p>Site under construction</p>
+    </body>
+</html>
