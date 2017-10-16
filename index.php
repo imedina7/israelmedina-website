ec942ac89f3f1a6d372f8c86ebf08b2e3b42273c
Israel Medina <isra2dave@gmail.com>
2017-10-16 03:18:56 -0300

    added social icons

--- index.php
+++ index.php
@@ -5,13 +5,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Israel Medina</title>
     <link rel="stylesheet" href="/static/css/master.css">
+    <link rel="stylesheet" href="/static/css/font-awesome.min.css">
     <link href="https://fonts.googleapis.com/css?family=Raleway|Roboto" rel="stylesheet">
   </head>
   <body>
     <div class="general">
       <div class="item">
         <h1>Israel Medina</h1>
-        <p>Site under construction</p>
+        <div class="social-wrap">
+          <a href="https://www.facebook.com/themarciano"><i class="fa fa-facebook" aria-hidden="true"><span>Facebook</span></i></a>
+          <a href="https://www.instagram.com/isra2dave"><i class="fa fa-instagram" aria-hidden="true"><span>Instagram</span></i></a>
+          <a href="https://www.linkedin.com/in/imedina7"><i class="fa fa-linkedin" aria-hidden="true"><span>LinkedIn</span></i></a>
+          <a href="https://www.soundcloud.com/imedina7"><i class="fa fa-soundcloud" aria-hidden="true"><span>LinkedIn</span></i></a>
+        </div>
       </div>
     </div>
     </body>
